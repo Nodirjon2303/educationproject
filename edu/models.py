cd4453d5c9ba4b7de2706a310a04081e3d19daad
Nodirjon Ro'zimurodov <82306944+Nodirjon2303@users.noreply.github.com>
2021-12-04 17:51:43 +0500

    model qismi tayyor bo'lib qoldi

---
+++ edu/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(upload_to='images')
+    age = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class Courses(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=125, null=True, blank=True)
+    image = models.ImageField(upload_to='cources', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=125, null=True, blank=True)
+    image = models.ImageField(upload_to='blogs', null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    created_data = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+# SUCCES STORY HOMEWORK
+
+
