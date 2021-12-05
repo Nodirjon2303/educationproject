ab1da79309e0eccfe3ff1aa0759fa24abb63f343
User <you@example.com>
2021-12-05 15:49:39 +0500

    templatelar ulandi

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse('salom')
+def index_view(request):
+    return render(request,'index.html')
